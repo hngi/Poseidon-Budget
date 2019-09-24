7d3b9812724dfbb077593ca39d425b9ffea32f6e
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-09-25 00:10:05 +0300

    working on dashboard

--- app/src/main/java/com/gradimut/poseidonbuget/MainActivity.java
+++ app/src/main/java/com/gradimut/poseidonbuget/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gradimut.poseidonbuget;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         myadapter = new SlideAdapter(this);
         viewPager.setAdapter(myadapter);
 
+
+        Intent reg_intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(reg_intent);
 
     }
 }
