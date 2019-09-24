8c3327edef34d7252ddaab511bb2b76ee9bd84b0
Gradia Mutambay <gradimutdev@gmail.com>
2019-09-24 09:28:38 +0300

    feature (slidepage): Adding the onboardin page
    I've added asset image
    I've created sliderPage class
    I've added slide view layout
    And made some modification to the MainActivy
    
    [finishes #168713515]

--- app/src/main/java/com/gradimut/poseidonbuget/MainActivity.java
+++ app/src/main/java/com/gradimut/poseidonbuget/MainActivity.java
@@ -1,13 +1,21 @@
 package com.gradimut.poseidonbuget;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private SlideAdapter myadapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        myadapter = new SlideAdapter(this);
+        viewPager.setAdapter(myadapter);
     }
 }
