a74b4f25253c5e4ce39e66a27cc5400d331dafd6
Victor Mba <victor.mba@outlook.com>
2019-09-25 22:05:04 +0100

    VictorM (#10)
    * Budget page UI
    
    * Bottom nav and removed title bar
    
    * Updated Bottom nav with buttons
    
    * Updated: Bottom nav with buttons

---
+++ app/src/main/java/com/gradimut/poseidonbuget/BudgetActivity.java
@@ -0,0 +1,32 @@
+package com.gradimut.poseidonbuget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_budget);
+
+        // setNavigationMenu();
+    }
+
+    /*private void setNavigationMenu() {
+
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_add_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_history_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_home_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_person_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+
+    }*/
+}
