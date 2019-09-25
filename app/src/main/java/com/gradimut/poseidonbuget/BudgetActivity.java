04a0f804461c5bd51292bda64d052de08fdf873d
Victor <victor.mba@outlook.com>
2019-09-25 02:12:40 +0100

    Updated Bottom nav with buttons

--- app/src/main/java/com/gradimut/poseidonbuget/BudgetActivity.java
+++ app/src/main/java/com/gradimut/poseidonbuget/BudgetActivity.java
@@ -9,5 +9,24 @@ public class BudgetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+
+        // setNavigationMenu();
     }
+
+    /*private void setNavigationMenu() {
+
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_add_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_history_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_home_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+        DrawableCompat.setTint(
+                DrawableCompat.wrap(getDrawable(R.drawable.ic_person_black_24dp)),
+                ContextCompat.getColor(this, R.color.black));
+
+    }*/
 }
