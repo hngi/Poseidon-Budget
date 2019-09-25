9a33fa28864412f001edd27991620052ff01922c
Victor <victor.mba@outlook.com>
2019-09-25 21:27:05 +0100

    Updated Bottom nav with buttons

---
+++ app/src/main/java/com/gradimut/poseidonbuget/DashBoardActivity.java
@@ -0,0 +1,37 @@
+package com.gradimut.poseidonbuget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+
+    ArrayList<DashModel> dashList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        rv = findViewById(R.id.recycler_history);
+
+        dashList = new ArrayList<>();
+        dashList.add(new DashModel(R.drawable.coins, "First salary", "24 October", "Naira", "200000"));
+        dashList.add(new DashModel(R.drawable.coins, "November salary", "2 November", "Naira", "100000"));
+        dashList.add(new DashModel(R.drawable.coins, "January salary", "24 January", "Naira", "200000"));
+        dashList.add(new DashModel(R.drawable.coins, "December salary", "03 December", "Naira", "200000"));
+        dashList.add(new DashModel(R.drawable.coins, "August salary", "24 August", "Naira", "200000"));
+        RecyclerView.LayoutManager rvLayout = new LinearLayoutManager(this);
+
+        rv.setLayoutManager(rvLayout);
+
+        DashAdapter adapter = new DashAdapter(this, dashList);
+        rv.setAdapter(adapter);
+
+    }
+}
---
+++ app/src/main/java/com/gradimut/poseidonbuget/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.gradimut.poseidonbuget;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private TextView txtWelcome;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        txtWelcome = findViewById(R.id.txtWelcome);
+        imageView = findViewById(R.id.imgLogo);
+
+
+        Animation _mainAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
+        Animation _skipAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_skip_anim);
+
+        txtWelcome.startAnimation(_skipAnimation);
+        imageView.startAnimation(_mainAnimation);
+
+
+        final Intent intent = new Intent(this, MainActivity.class);
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(6000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
