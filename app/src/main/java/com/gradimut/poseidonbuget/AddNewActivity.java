d42c9533adb7e0205bb51eb0216f10076a5284f4
Gradia Mutambay <gradimutdev@gmail.com>
2019-09-27 23:09:21 +0300

    Fixing AddBudgetActivity

--- app/src/main/java/com/gradimut/poseidonbuget/AddNewActivity.java
+++ app/src/main/java/com/gradimut/poseidonbuget/AddNewActivity.java
@@ -3,14 +3,21 @@ package com.gradimut.poseidonbuget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddNewActivity extends AppCompatActivity {
+
+    private AppCompatImageView mAddbugdet;
+    private EditText one, two, three;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+
     }
 
     public void profileBtn_onClick(View view) {
